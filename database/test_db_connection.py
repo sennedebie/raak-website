21bf1e26ae520a5cccd0bac8ded1dc180216b2e4
sennedebie <sennedebie@icloud.com>
2025-05-30 19:16:10 +0200

    add comments for documentation

---
+++ database/test_db_connection.py
@@ -0,0 +1,24 @@
+# ════════════════════════════════════════════════
+# ▶ IMPORTS
+# ════════════════════════════════════════════════
+
+import os
+import psycopg2
+
+
+# ════════════════════════════════════════════════
+# ▶ CHECK DB CONNECTION
+# ════════════════════════════════════════════════
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+try:
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+    cur.execute("SELECT 1;")
+    print("Database connection successful!")
+    cur.close()
+    conn.close()
+except Exception as e:
+    print("Database connection failed:")
+    print(e)
